4106bbe62dfb3e446bd53bd457378fc29f5f17fe
Lukah Dykes <dyke0005@flinders.edu.au>
2016-09-02 19:14:41 +0930

    v0.8.5c

--- IgFamily.h
+++ IgFamily.h
@@ -33,8 +33,8 @@ namespace IgFamily {
 	const bool OUTPUT_FASTA = 1;
 	const bool BLASTP_BY_SELECTED_PEPTIDE = 1;
 
-	const string IGFAMILY_ROOT_DIR = "IgFamily_root_4.txt";
-	const string DEFAULT_IGFAMILY_DIRECTORY = "C:\\Users\\LJ\\IgFamily\\";
+	const string IGFAMILY_ROOT_DIR = "IgFamily_root_9.txt";
+	const string DEFAULT_IGFAMILY_DIRECTORY = "Z:\\Lukah_Dykes\\IgFamily\\";
 	const string DEFAULT_INPUT_FASTA_DIRECTORY = DEFAULT_IGFAMILY_DIRECTORY + "FASTA\\";
 	const string DEFAULT_BLASTP_DIRECTORY = DEFAULT_IGFAMILY_DIRECTORY + "blast_directory\\";
 	const string DEFAULT_MSCONVERT_DIRECTORY = DEFAULT_IGFAMILY_DIRECTORY + "msconvert\\";
