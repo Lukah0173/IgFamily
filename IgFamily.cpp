f84bb50b696310326a93cae6bc2a4c6206456cdd
Lukah Dykes <dyke0005@flinders.edu.au>
2016-09-25 21:27:54 +0930

    v0.9.9b

--- IgFamily.cpp
+++ IgFamily.cpp
@@ -1,4 +1,4 @@
-// * * IgFamily v0.9.9a * * 
+// * * IgFamily v0.9.9b * * 
 //
 // Lukah Dykes - Flinders Proteomics Facility - 2016
 // 
--- IgFamily.h
+++ IgFamily.h
@@ -16,7 +16,7 @@ namespace IgFamily {
 
 	using std::string;
 
-	const string version{ "v0.9.9a" };
+	const string version{ "v0.9.9b" };
 
 	const bool FILESYSTEM_MODE{ 1 };
 	const bool FILESYSTEM_UPDATE_ALL{ 1 };
