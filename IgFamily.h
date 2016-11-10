ace573f62f43a92d6034955942fbec1abad7003b
Lukah0173 <dyke0005@flinders.edu.au>
2016-11-10 23:16:39 +1030

    v0.12.2k

--- IgFamily.h
+++ IgFamily.h
@@ -17,7 +17,7 @@ namespace IgFamily {
 
 	using std::string;
 
-	const string version{ "v0.12.2j" };
+	const string version{ "v0.12.2k" };
 
 	const bool FILESYSTEM_MODE{ 1 };
 	const bool FILESYSTEM_UPDATE_ALL{ 1 };
